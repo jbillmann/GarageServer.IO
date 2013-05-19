fda56a0e75a09b6fe9ad74ef05f73da1a4571dad
Jeremiah Billmann <jcbillmann@yahoo.com>
2013-05-19 15:46:26 -0400

    Fixed client path

--- example/app.js
+++ example/app.js
@@ -23,6 +23,7 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  app.use(express.static(path.join(__dirname, '..', 'client')));
 });
 
 app.configure('development', function(){
