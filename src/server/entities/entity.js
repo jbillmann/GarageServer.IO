2af51012c1f3f3bdc6f74d07bb67e869d095750b
Jeremiah Billmann <jcbillmann@yahoo.com>
2013-07-16 21:21:44 -0400

    Created separate history class

--- src/server/entities/entity.js
+++ src/server/entities/entity.js
@@ -1,3 +1,5 @@
+var history = require('./history');
+
 exports = module.exports = Entity;
 
 function Entity (id) {
@@ -10,12 +12,13 @@ function Entity (id) {
 Entity.prototype = {
     addState: function (state, executionTime) {
         this.addHistory(state, executionTime);
+        this.state = state;
         this.sequence += 1;
     },
     addHistory: function (state, executionTime) {
-        var minTime, spliceTo = 0;
-        this.state = state;
-        this.stateHistory.push({ state: state, executionTime: executionTime });
+        var minTime, spliceTo = 0, newHistory = new history(state, executionTime);
+
+        this.stateHistory.push(newHistory);
         minTime = this.stateHistory[this.stateHistory.length - 1].executionTime - 1000;
 
         for (var i = 0; i < this.stateHistory.length; i ++) {
---
+++ src/server/entities/history.js
@@ -0,0 +1,6 @@
+exports = module.exports = History;
+
+function History (state, executionTime) {
+    this.state = state;
+    this.executionTime = executionTime;
+}
