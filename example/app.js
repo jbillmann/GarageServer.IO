2564f3492fa6b67eb4123ad8f1c22e710b5fb7c3
Jeremiah Billmann <jcbillmann@yahoo.com>
2013-08-09 15:17:17 -0400

    Updated docs

--- example/app.js
+++ example/app.js
@@ -7,7 +7,7 @@ var express = require('express'),
     app = express();
 
 app.configure(function () {
-    app.set('port', process.env.PORT || 2121);
+    app.set('port', process.env.PORT || 3000);
     app.set('views', __dirname + '/views');
     app.set('view engine', 'jade');
     app.use(express.favicon());
